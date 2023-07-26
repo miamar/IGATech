f484edf82a737241402b1aea93172a9e5a3d8378
miamar <mia.markanovic16@gmail.com>
2023-07-26 17:47:00 +0200

    new project

---
+++ static/js/gallery.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".gallery", {
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
